23fd022488e014123a3e595bf04a36f51b0b60ff
Jesus Ruescas Junior <jesus.junior.1999@hotmail.com>
2023-03-30 16:30:17 -0300

    Create a Liferay MVC Portlet Module

---
+++ training-workspace/modules/mvc-portlet-module/src/main/java/com/liferay/training/portletmodule/portlet/constants/SimpleMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.portletmodule.portlet.constants;
+
+/**
+ * @author safira-cli
+ */
+public class SimpleMVCPortletKeys {
+
+	public static final String SIMPLEMVC =
+		"com_liferay_training_portletmodule_portlet_SimpleMVCPortlet";
+
+}
